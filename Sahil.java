eb8d298583ded32b8d99f600e81e87c9f751dd0b
sahilqa <68937517+sahilqa@users.noreply.github.com>
2020-07-29 15:24:43 +0530

    initial comment

---
+++ Sahil.java
@@ -0,0 +1,6 @@
+public class Sahil {
+    public static void main(String args[]){
+        System.out.println("Hello Java");
+    }
+}
+
---
+++ Switch.java
@@ -0,0 +1,22 @@
+public class Switch {
+    public static void main(String args[]) {
+
+            int number = 0;
+            //switch expression with int value
+            switch (number) {
+                //switch cases without break statements
+                case 10:
+                    System.out.println("10");
+                case 20:
+                    System.out.println("20");
+                case 30:
+                    System.out.println("30");
+                default:
+                    System.out.println("Not in 10, 20 or 30");
+            }
+        }
+    }
+
+
+
+
